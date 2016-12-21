0bd84bbfe1ae38623b5b6e599ec5b41ee5614f3b
Imed Eddine Nezzar <imad.eddine.nezzar@gmail.com>
2016-12-21 16:09:01 +0100

    implemented istream_ingore

--- students_db/includes/util.h
+++ students_db/includes/util.h
@@ -1,6 +1,7 @@
 #ifndef __INCLUDES_UTIL_H__
 #define __INCLUDES_UTIL_H__
 
+#include <stdio.h>        // FILE
 #include <string.h>       // memcpy
 #include <stdlib.h>       // malloc
 #include <stddef.h>       // size_t
@@ -12,6 +13,13 @@ void* memdup(const void* addr, size_t n) {
   void* cp = malloc(n);
   memcpy(cp, addr, n);
   return cp;
+}
+
+/** @fn void isflush(FILE* instream)
+ *  @brief
+ */
+void istream_ignore(FILE* instream, int d) {
+  for (int c; (c = fgetc(instream)) != EOF && c != d;);
 }
 
 #endif // __INCLUDES_UTIL_H__
